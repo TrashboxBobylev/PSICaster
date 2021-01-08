1e096d4825320d85d488cf48f844dbd4abb3abcb
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-01-08 20:31:27 +0300

    1.0: editing metadata

---
+++ src/main/java/com/trashboxbobylev/psicaster/PSICaster.java
@@ -0,0 +1,11 @@
+package com.trashboxbobylev.psicaster;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = PSICaster.MODID, name = PSICaster.NAME, version = PSICaster.VERSION)
+public class PSICaster
+{
+    public static final String MODID = "psicaster";
+    public static final String NAME = "PSICaster";
+    public static final String VERSION = "1.0";
+}
